46a98c2bdc7eaac22f18f01d8b0b3ca751ac3d54
Code Sandbox <hello@codesandbox.io>
2022-04-15 22:47:59 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.51be1aebc2ab73c8f9b8397f7771e3a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5f04a06db6ab77b5da0fa7f07a43bb",
+    "url": "https://alirezaac.github.io/csb-vs24e0/index.html"
+  },
+  {
+    "revision": "fbd7d0e8023c90c87f80",
+    "url": "https://alirezaac.github.io/csb-vs24e0/static/css/main.7e8f607e.chunk.css"
+  },
+  {
+    "revision": "db72e7dcc1da06a0e79b",
+    "url": "https://alirezaac.github.io/csb-vs24e0/static/js/2.4eb410be.chunk.js"
+  },
+  {
+    "revision": "fbd7d0e8023c90c87f80",
+    "url": "https://alirezaac.github.io/csb-vs24e0/static/js/main.5ab5e9b0.chunk.js"
+  },
+  {
+    "revision": "75c172b913c2967fbe8e",
+    "url": "https://alirezaac.github.io/csb-vs24e0/static/js/runtime~main.8ad99bb1.js"
+  }
+]);
